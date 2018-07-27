d7bd0e5b3a6f978ee610fc8b8d9084489965c042
zhangzhentao <28299209@qq.com>
2018-07-27 21:57:32 +0800

    collect all playlist

--- playlist163.py
+++ playlist163.py
@@ -9,7 +9,8 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36"
 chrome_options.add_argument('--user-agent=' + user_agent)
-
+prefs = {"profile.managed_default_content_settings.images":2}
+chrome_options.add_experimental_option("prefs", prefs)
 chrome_options.binary_location = r'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
 # chrome_options.binary_location = '/opt/google/chrome/chrome'
 
@@ -27,9 +28,11 @@ while page_url != 'javascript:void(0)':
         data = web.find_element_by_id('m-pl-container').find_elements_by_tag_name('li')
         for i in range(len(data)):
             nb = data[i].find_element_by_class_name('nb').text
-            if '万' in nb and int(nb.split("万")[0]) > 500:
-                msk = data[i].find_element_by_css_selector('a.msk')
-                writer.writerow([msk.get_attribute('title'), nb, msk.get_attribute('href')])
+            # if '万' in nb and int(nb.split("万")[0]) > 500:
+            if '万' in nb:
+                nb = int(nb.split("万")[0]) * 10000
+            msk = data[i].find_element_by_css_selector('a.msk')
+            writer.writerow([msk.get_attribute('title'), nb, msk.get_attribute('href')])
     except Exception as e:
         print("Exception Found: " + str(e))
         continue
